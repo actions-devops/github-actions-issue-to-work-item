607605a4a1773e1cad89a400d1b59444f793c22d
Aaron Barth <aaronba@microsoft.com>
2020-06-02 21:20:14 -0400

    More logging and testing..

--- index.js
+++ index.js
@@ -472,11 +472,12 @@ function getValuesFromPayload(payload, env) {
 		//action: payload.action != undefined ? payload.action : "",
 		//url: payload.issue.html_url != undefined ? payload.issue.html_url : "",
 		//number: payload.issue.number != undefined ? payload.issue.number : -1,
+		number = 1,
 		//title: payload.issue.title != undefined ? payload.issue.title : "",
 		//state: payload.issue.state != undefined ? payload.issue.state : "",
 		//user: payload.issue.user.login != undefined ? payload.issue.user.login : "",
 		//body: payload.issue.body != undefined ? payload.issue.body : "",
-		//repo_fullname: payload.repository.full_name != undefined ? payload.repository.full_name : "",
+		repo_fullname: payload.repository.full_name != undefined ? payload.repository.full_name : "",
 		//repo_name: payload.repository.name != undefined ? payload.repository.name : "",
 		//repo_url: payload.repository.html_url != undefined ? payload.repository.html_url : "",
 		//closed_at: payload.issue.closed_at != undefined ? payload.issue.closed_at : null,
@@ -484,8 +485,8 @@ function getValuesFromPayload(payload, env) {
 		//label: "",
 		//comment_text: "",
 		//comment_url: "",
-		//organization: "",
-		//repository: "",
+		organization: "",
+		repository: "",
 		sha : payload.sha,
 		env: {
 			organization: env.ado_organization != undefined ? env.ado_organization : "",
@@ -503,16 +504,16 @@ function getValuesFromPayload(payload, env) {
 	console.log(sha);
 	console.log("before payload.label");
 	// label is not always part of the payload
-	if (payload.label != undefined) {
-		vm.label = payload.label.name != undefined ? payload.label.name : "";
-	}
+	//if (payload.label != undefined) {
+	//	vm.label = payload.label.name != undefined ? payload.label.name : "";
+	//}
 
 	// comments are not always part of the payload
 	// prettier-ignore
-	if (payload.comment != undefined) {
-		vm.comment_text = payload.comment.body != undefined ? payload.comment.body : "";
-		vm.comment_url = payload.comment.html_url != undefined ? payload.comment.html_url : "";
-	}
+	//if (payload.comment != undefined) {
+	//	vm.comment_text = payload.comment.body != undefined ? payload.comment.body : "";
+	//	vm.comment_url = payload.comment.html_url != undefined ? payload.comment.html_url : "";
+	//}
 
 	// split repo full name to get the org and repository names
 	if (vm.repo_fullname != "") {
@@ -520,6 +521,6 @@ function getValuesFromPayload(payload, env) {
 		vm.organization = split[0] != undefined ? split[0] : "";
 		vm.repository = split[1] != undefined ? split[1] : "";
 	}
-
+	console.log("end of getValuesFromPayload()");
 	return vm;
 }
